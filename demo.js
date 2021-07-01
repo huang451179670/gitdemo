cda473841793739e1dce1c2c54922a1ae6725a51
xuanpenghuang <39343888+huang451179670@users.noreply.github.com>
2021-07-01 13:58:07 +0800

    feat: main test code updated
    feat: dev test code updated

--- demo.js
+++ demo.js
@@ -3,3 +3,21 @@ console.log(122222222221222000023332221121);
 const a = ()=>{
   let test = ""
 }
+
+const b = ()=> {
+  let test = '';
+}
+const c = ()=> {
+  let test = '';
+}
+const d = ()=> {
+  let test = '';
+}
+
+const e = ()=>{
+  let test = ""
+}
+
+const f = ()=>{
+  let test = ""
+}
